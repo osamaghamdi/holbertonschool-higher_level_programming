2817a90f2c090ead43676dd993deca9b0028f375
Your Name <you@example.com>
2025-05-08 10:34:57 +0000

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{i:02}", end=", " if i < 99 else "")
+    if i < 99:
+        print("{:02},".format(i), end=" ")
+    else:
+        print("{:02}".format(i))
