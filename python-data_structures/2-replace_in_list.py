223ce958cfd02e11b703fcd3c9c4842b0b5497f4
Your Name <you@example.com>
2025-05-12 15:14:28 +0000

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, new_element):
-    if 0 <= idx < len(my_list):
-        new_list = my_list.copy()
-        new_list[idx] = new_element
-        return new_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = new_element
     return my_list
