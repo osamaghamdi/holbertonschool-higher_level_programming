f268c98aa6e73dd27556af90ef941959c5daaa95
Your Name <you@example.com>
2025-05-16 11:46:12 +0000

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
-    
+
     roman_dict = {
         'I': 1,
         'V': 5,
@@ -12,10 +12,10 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    
+
     total = 0
     prev_value = 0
-    
+
     for char in reversed(roman_string):
         value = roman_dict.get(char, 0)
         if value < prev_value:
@@ -23,5 +23,5 @@ def roman_to_int(roman_string):
         else:
             total += value
         prev_value = value
-    
+
     return total
