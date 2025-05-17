fa34ad991a3bd972be586f167ce6f5e74e9065e0
Your Name <you@example.com>
2025-05-17 15:18:21 +0000

    edit

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -17,10 +17,10 @@ def say_my_name(first_name, last_name=""):
         TypeError: If first_name or last_name is not a string
     """
 
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
 
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name).strip())
+    print("My name is {} {}".format(first_name, last_name))
