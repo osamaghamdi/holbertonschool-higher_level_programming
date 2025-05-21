b1c56bca7785a107db808d0f93337babcbd81475
Your Name <you@example.com>
2025-05-21 10:58:48 +0300

    add file 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size):
+        """Initialize the square with a size."""
+        self.__size = size
