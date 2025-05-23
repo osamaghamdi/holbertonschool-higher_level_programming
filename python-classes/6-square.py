db3071ccb8e985669e8337fc2d9f2c5b597d8ae1
Your Name <you@example.com>
2025-05-23 20:17:17 +0300

    update

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -78,7 +78,7 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print("-" * self.__position[0] + "#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
                 
     @property
     def position(self):
