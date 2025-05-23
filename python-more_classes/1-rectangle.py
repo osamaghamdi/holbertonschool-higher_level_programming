1a5ad7e7877e17957089d0c10d93175c13baae88
Your Name <you@example.com>
2025-05-23 14:13:48 +0300

    update

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,8 +17,17 @@ class Rectangle:
             TypeError: If width or height is not an integer.
             ValueError: If width or height is less than 0.
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
+        
 
     @property
     def width(self):
