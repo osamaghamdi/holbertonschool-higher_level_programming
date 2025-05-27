6e738f6d90652403f25125ee0171b3533d85b6a6
Your Name <you@example.com>
2025-05-27 15:55:03 +0300

    create file 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """A class BaseGeometry with methods for geometric calculations."""
+    def area(self):
+        """Calculates the area of a geometry."""
+        raise Exception("area() is not implemented")
