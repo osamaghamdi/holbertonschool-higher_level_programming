7c92683d2c801634c3d0b84c09d9821e6c6ef6b0
Your Name <you@example.com>
2025-05-28 10:59:34 +0300

    create file 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that includes a class Square that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that defines a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a square with a given size."""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
