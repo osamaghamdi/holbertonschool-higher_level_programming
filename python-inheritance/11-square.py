3f1cd79e488355c2149dd54674b2a1f60e0ebc10
Your Name <you@example.com>
2025-05-28 11:15:47 +0300

    update

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
     def area(self):
         """Calculates the area of the square."""
         return self.__size ** 2
-    
+
     def __str__(self):
         """Returns a string representation of the square."""
         return f"[Square] {self.__size}/{self.__size}"
