c737a4487593b24a9375c64b77fb2469e64f545b
Your Name <you@example.com>
2025-06-02 15:32:42 +0300

    update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 
 
 class MyList(list):
-
+    """A class that inherits from list and adds a method to print sorted."""
     def print_sorted(self):
         """Prints the list, sorted in ascending order."""
         print(sorted(self))
