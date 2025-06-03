1237caaf021236e5b10c74e425add657e20091b8
Your Name <you@example.com>
2025-06-03 15:06:28 +0300

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module to read a text file and print its content to stdout."""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
