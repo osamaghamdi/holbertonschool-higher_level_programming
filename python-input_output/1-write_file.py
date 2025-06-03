f7ab9205e36e12e59e85feba7243220de2cc6554
Your Name <you@example.com>
2025-06-03 15:14:33 +0300

    update

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """Writes a string to a text file (UTF8) and returns 
+    the number of characters written."""
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
