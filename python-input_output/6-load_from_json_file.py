782d0a910623e109437871dd1e84e8df5f40bcdd
Your Name <you@example.com>
2025-06-12 18:44:16 +0300

    update

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Dfine a JISON file reading function."""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file."""
+    with open(filename) as file:
+        return json.load(file)
