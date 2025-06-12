0072d1718adc919753976bf10b5884910b4abe8c
Your Name <you@example.com>
2025-06-12 18:38:45 +0300

    update

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Define a function that converts an object to a JSON string."""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object."""
+    return json.dumps(my_obj)
