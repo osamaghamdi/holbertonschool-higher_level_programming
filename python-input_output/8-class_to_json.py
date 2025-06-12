5fdf489fc563eeb5d0c28952a93a786fc5ac49e7
Your Name <you@example.com>
2025-06-12 18:56:59 +0300

    update

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Dfine a Python class to JSON function."""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure."""
+    return obj.__dict__
