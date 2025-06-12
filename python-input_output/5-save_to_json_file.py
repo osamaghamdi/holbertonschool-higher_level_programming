8f91f9faaba9771cd84d7538e5c302ad11da4f34
Your Name <you@example.com>
2025-06-12 18:42:09 +0300

    update

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Dfine a JSON file writing function."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using JSON representation."""
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
