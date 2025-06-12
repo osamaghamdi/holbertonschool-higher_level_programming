6855fee23aa66ad949c8f5d1fa70cb3bb453e093
Your Name <you@example.com>
2025-06-12 16:08:46 +0300

    update

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a file appending function."""
 
+
 def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file."""
     with open(filename, 'a', encoding='utf-8') as file:
