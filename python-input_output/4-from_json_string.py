d8e7466519846bcb512dcce5880a4f2c9943dd78
Your Name <you@example.com>
2025-06-12 18:39:36 +0300

    update

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Define a function that converts a JSON string to an object."""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the object represented by a JSON string."""
+    return json.loads(my_str)
