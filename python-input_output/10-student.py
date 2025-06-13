6430243924c4145acda0ab8fce14b5bc03db3653
Your Name <you@example.com>
2025-06-13 13:53:58 +0300

    update

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Dfine a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
