0d9b62eba1455a923fe36bb3ae0f54fcdaf7660d
Your Name <you@example.com>
2025-06-14 10:26:54 +0300

    make fole 1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Dfine a CustomClass that can be serialized and deserialized using pickle."""
+import pickle
+
+
+class CustomClass:
+    """A custom class with a simple attribute."""
+    def __init__(self, value):
+        """Initialize the class with a value."""
+        self.value = value
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+        
+    def dislay(self):
+        """Display the attributes of the class."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+        
+def serialize(self, filename):
+    """Serialize the object to a file using pickle."""
+    with open(filename, 'wb') as file:
+        pickle.dump(self, file)
+
+@classmethod
+def deserialize(cls, filename):
+    """Deserialize the object from a file using pickle."""
+    try:
+        with open(filename, 'rb') as f:
+            obj = pickle.load(f)
+            if isinstance(obj, cls):
+                return obj
+            return None
+    except (FileNotFoundError, pickle.UnpicklingError, EOFError):
+        return None
