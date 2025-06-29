1bad702014a603ee90896845987921b9609e2a73
Your Name <you@example.com>
2025-06-29 20:55:27 +0300

    update

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,13 +9,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    # Connect to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     session = Session(engine)
 
-    # Query and print all states ordered by id
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
 
