811549f3acfcf5fe1c76f2aff4326fc8fb8ddbef
Your Name <you@example.com>
2025-07-18 14:13:40 +0300

    update

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,8 @@
+fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('hello').textContent = data.hello;
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
